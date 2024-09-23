3af2d059dc4d4e08668e6cc20bb9dfaf1e63beef
Giovalb-CS <g.bonacci1@studenti.unisa.it>
2024-09-23 12:18:02 +0200

    initialization commit

---
+++ src/main/java/com/example/rigbuilder_webapp_project/HelloServlet.java
@@ -0,0 +1,20 @@
+package com.example.rigbuilder_webapp_project;
+
+import java.io.*;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
