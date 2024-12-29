fb7b4ada41d69cd1b4fdbc7e3f728724ff03ef03
Giovalb-CS <145759384+Giovalb-CS@users.noreply.github.com>
2024-12-29 22:40:53 +0100

    configurazione azure db

--- src/main/java/model/ConPool.java
+++ src/main/java/model/ConPool.java
@@ -5,7 +5,6 @@ import org.apache.tomcat.jdbc.pool.PoolProperties;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.TimeZone;
 
 public class ConPool {
     private static DataSource datasource;
@@ -13,10 +12,10 @@ public class ConPool {
     public static Connection getConnection() throws SQLException {
         if (datasource == null) {
             PoolProperties p = new PoolProperties();
-            p.setUrl("jdbc:mysql://voe2xwwmdq2q95ow:mt9qw738xn5w73x5@jfrpocyduwfg38kq.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/p5uucxui7h661tgv");
+            p.setUrl("jdbc:mysql://rigbuilder-server.mysql.database.azure.com:3306/rigbuilder-database");
             p.setDriverClassName("com.mysql.cj.jdbc.Driver");
-            p.setUsername("voe2xwwmdq2q95ow");
-            p.setPassword("mt9qw738xn5w73x5");
+            p.setUsername("culptmjfml");
+            p.setPassword("Rigbuilder123@");
             p.setMaxActive(100);
             p.setInitialSize(10);
             p.setMinIdle(10);
